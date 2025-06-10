ae702102a964963bc93ec92abd8dfab91f9d87e4
Samuvel Reegan <samuelreegan372@gmail.com>
2025-06-10 22:26:10 +0530

    Add two Pointer in target value

---
+++ Two Pointer/TwoPointer.py
@@ -0,0 +1,18 @@
+arr = [2,3,4,5,6,7,10]
+
+target = 11
+
+left = 0
+
+right = len(arr)-1
+
+while (left<right):
+    heap = arr[left] + arr[right]
+
+    if heap == target:
+        print( left , right)
+        break
+    elif heap < target:
+        left = left +1
+    else:
+        right = right -1
